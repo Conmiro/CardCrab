e55e0cc578a8f87c5191ed522767c2edce9d2167
Conmiro <conmiro@live.com>
2017-01-10 16:39:56 -0600

    added models,views.py and templates

--- DjangoTut/DjangoTut/settings.py
+++ DjangoTut/DjangoTut/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'DjangoTut.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,6 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },

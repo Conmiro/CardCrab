e48ca62abccf3c4d711e0f5c86b916f51a298387
Connor Romeros <conmiro@live.com>
2017-03-18 02:33:07 -0500

    added ability to change pages with arrow keys

--- CardCrab/static/js/search.js
+++ CardCrab/static/js/search.js
@@ -1,7 +1,24 @@
 
 
-function changePage(pageNum) {
+$(document).keydown(function(e) {
+    console.log(e.which)
+    switch(e.which) {
+        case 37: //left
+        changePage($('#prevpage').val())
+        break;
 
+        case 39: //right
+        changePage($('#nextpage').val())
+        break;
+
+        default:
+        return;
+    }
+
+});
+
+function changePage(pageNum) {
+    console.log(pageNum)
     $("#page").val(pageNum)
     document.getElementById("change_page_form").submit()
 

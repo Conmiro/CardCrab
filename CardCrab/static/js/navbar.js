840196fc4be3bd97ada50006c4ea44d2543aed0c
Connor Romeros <conmiro@live.com>
2017-03-20 01:08:35 -0500

    added javascript to highlight page tab

--- CardCrab/static/js/navbar.js
+++ CardCrab/static/js/navbar.js
@@ -1,1 +1,12 @@
 
+$(document).ready(function() {
+
+    var path = $('#current_path').val()
+    console.log(path)
+    if (path=="/"){
+        $('#home_label').addClass('active')
+    }else if (path=="/search/") {
+        $('#cards_label').addClass('active')
+    }
+
+})
